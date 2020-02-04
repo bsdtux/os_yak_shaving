268a039ae715dfabd9aed03d3c6aa2980b18d33f
Josh Stephens <bsdtux@gmail.com>
2020-02-03 23:42:57 -0600

    Adds updated install.sh file

--- freebsd/install.sh
+++ freebsd/install.sh
@@ -3,7 +3,9 @@
 su -m root -c 'pkg install -y git-lite vim zsh doas xorg nvidia-driver nvidia-settings nvidia-xconfig'
 su -m root -c 'cp ./doas.conf /usr/local/etc/doas.conf'
 
-mkdir -p $HOME/src
+if [ ! -d $HOME/src ]; then
+	mkdir -p $HOME/src
+fi
 
 if [ ! -d $HOME/src/dwm ]; then
 	git clone git://git.suckless.org/dwm $HOME/src/dwm
@@ -13,5 +15,12 @@ if [ ! -d $HOME/src/st ]; then
 	git clone git://git.suckless.org/st $HOME/src/st
 fi
 
+if [ "$SHELL" != "/usr/local/bin/zsh" ]; then
+	chsh -s /usr/local/bin/zsh
+fi
 
-chsh -s /usr/local/bin/zsh
+su -m root -c 'sysrc linux_enable="YES"'
+
+if [ ! -f $HOME/.zshrc ]; then
+	cp ./.zshrc $HOME/.zshrc
+fi
