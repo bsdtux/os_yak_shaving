e6edd4e11aef183f4a599a8f2e56feb61c489fb7
Josh Stephens <bsdtux@gmail.com>
2021-12-30 11:02:51 -0600

    Moves dotfiles to viewable files. Updates terminal script to link to files

--- dotfiles/terminal_setup.sh
+++ dotfiles/terminal_setup.sh
@@ -4,8 +4,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-cp .tmux.conf ~/
-cp .zshrc ~/
+ln -sf ./tmux.conf $HOME/.tmux.conf
+ln -sf ./zshrc $HOME/.zshrc
 
 mkdir -p $HOME/.fonts
 cp -r ./*.ttf $HOME/.fonts/
